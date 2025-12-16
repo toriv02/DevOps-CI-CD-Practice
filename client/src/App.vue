07be2afd32f93f3a9e1f5f802be64482b33494a6
toriv <torivmalahov@gmail.com>
2025-12-16 12:47:09 +0800

    test

--- client/src/App.vue
+++ client/src/App.vue
@@ -27,7 +27,7 @@ const isActive = (routePath) => {
   <div class="container">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/"> Пример</a>
+        <a class="navbar-brand" href="/"> Пример dev</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
